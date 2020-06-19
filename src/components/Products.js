4719eedf91e150f343a9c288c8149d1a8b11b600
tsungtingdu <tsungtingdu@gmail.com>
2020-06-19 14:39:31 +0800

    get products data from API

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,50 +1,21 @@
 import React, { Fragment } from "react";
+import axios from "commons/axios";
 import ToolBox from "components/ToolBox";
 import Product from "components/Product";
 
 class Products extends React.Component {
-  products = [
-    {
-      id: 1,
-      name: "Shoes",
-      tags: "color",
-      image: "images/1.jpg",
-      price: 199,
-      status: "available",
-    },
-    {
-      id: 2,
-      name: "Shoes",
-      tags: "color",
-      image: "images/2.jpg",
-      price: 299,
-      status: "available",
-    },
-    {
-      id: 3,
-      name: "Shoes",
-      tags: "color",
-      image: "images/3.jpg",
-      price: 399,
-      status: "available",
-    },
-    {
-      id: 4,
-      name: "Shoes",
-      tags: "color",
-      image: "images/4.jpg",
-      price: 499,
-      status: "available",
-    },
-    {
-      id: 5,
-      name: "Shoes",
-      tags: "color",
-      image: "images/5.jpg",
-      price: 599,
-      status: "unavailable",
-    },
-  ];
+  state = {
+    products: [],
+  };
+
+  componentDidMount() {
+    axios.get("/products").then((res) => {
+      this.setState({
+        products: res.data,
+      });
+    });
+  }
+
   render() {
     return (
       <Fragment>
@@ -52,7 +23,7 @@ class Products extends React.Component {
           <ToolBox />
           <div className="products">
             <div className="columns is-multiline is-desktop">
-              {this.products.map((d) => {
+              {this.state.products.map((d) => {
                 return (
                   <div className="column is-3" key={d.id}>
                     <Product product={d} />
