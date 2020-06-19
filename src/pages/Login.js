ace932fbd302bd95619d47543b4f8a73c87d02cb
tsungtingdu <tsungtingdu@gmail.com>
2020-06-19 10:55:52 +0800

    get user data from login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,69 @@
 import React, { Fragment } from "react";
 
 class Login extends React.Component {
+  // state example
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     isLike: false,
+  //   };
+  // }
+
+  // ref example
+  // emailRef = React.createRef();
+  // passwordRef = React.createRef();
+
+  // state
+  state = {
+    email: "",
+    password: "",
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // const formData = {
+    //   email: this.emailRef.current.value,
+    //   password: this.passwordRef.current.value,
+    // };
+    // console.log(formData);
+
+    // show form data
+    console.log(this.state);
+    // redirect
+    this.props.history.push("/");
+  };
+
+  handleChange = (e) => {
+    console.log(e.target);
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
   render() {
     return (
       <Fragment>
         <div className="login-wrapper">
-          <form action="" className="box login-box">
+          <form
+            action=""
+            className="box login-box"
+            onSubmit={this.handleSubmit}
+          >
             <p>Login</p>
             <div className="field">
               <label htmlFor="" className="label">
                 Email
               </label>
               <div>
-                <input type="text" placeholder="Email" className="input" />
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="input"
+                  name="email"
+                  // ref={this.emailRef}
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
               </div>
             </div>
             <div className="field">
@@ -20,7 +71,15 @@ class Login extends React.Component {
                 Password
               </label>
               <div>
-                <input type="text" placeholder="Password" className="input" />
+                <input
+                  type="text"
+                  placeholder="Password"
+                  className="input"
+                  name="password"
+                  // ref={this.passwordRef}
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
               </div>
             </div>
             <div className="control">
