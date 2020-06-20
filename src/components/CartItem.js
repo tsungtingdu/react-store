76f827da4012efd55e0adbdd394d7c0890552802
tsungtingdu <tsungtingdu@gmail.com>
2020-06-20 16:38:14 +0800

    complete actions in Cart page

---
+++ src/components/CartItem.js
@@ -0,0 +1,59 @@
+import React, { useState, useMemo } from "react";
+import axios from "commons/axios";
+
+const CartItem = (props) => {
+  const { id, name, image, price } = props.cart || {};
+  const [mount, setMount] = useState(props.cart.mount);
+
+  const handleChange = (e) => {
+    const _mount = parseInt(e.target.value);
+    setMount(_mount);
+    const newCart = {
+      ...props.cart,
+      mount: _mount,
+    };
+    axios.put(`/carts/${id}`, newCart).then((res) => {
+      props.updateCart(newCart);
+    });
+  };
+
+  const deleteCart = () => {
+    axios.delete(`/carts/${id}`).then((res) => {
+      props.deleteCart(props.cart);
+    });
+  };
+
+  // Only execute when price or mount changes
+  // const sumPrice = useMemo(() => {
+  //   return formatPrice(mount * parseInt(price));
+  // }, [mount, price]);
+
+  return (
+    <div className="columns is-vcentered">
+      <div className="column is-narrow" onClick={deleteCart}>
+        <span className="close">x</span>
+      </div>
+      <div className="column is-narrow">
+        <img src={image} alt={name} width="100" />
+      </div>
+      <div className="column cart-name is-narrow">{name}</div>
+      <div className="column">
+        <span className="price">{price}</span>
+      </div>
+      <div className="column">
+        <input
+          type="number"
+          className="input num-input"
+          min={1}
+          value={mount}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="column">
+        <span className="sum-price">{mount * price}</span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,12 +1,62 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Layout from "Layout";
+import CartItem from "components/CartItem";
+import axios from "commons/axios";
 
-const Cart = () => (
-  <Layout>
-    <div className="cart-page">
-      <p className="title has-text-centered">Cart</p>
-    </div>
-  </Layout>
-);
+// use React hook instead of class
+const Cart = () => {
+  const [carts, setCarts] = useState([]);
+
+  useEffect(() => {
+    axios.get("/carts").then((res) => setCarts(res.data));
+  }, []);
+
+  const totalPrice = useMemo(() => {
+    const _totalPrice = carts
+      .map((d) => d.mount * d.price)
+      .reduce((a, value) => a + value, 0);
+    return _totalPrice;
+  }, [carts]);
+
+  const updateCart = (cart) => {
+    const newCarts = [...carts];
+    const _index = newCarts.findIndex((d) => d.id === cart.id);
+    newCarts.splice(_index, 1, cart);
+    setCarts(newCarts);
+  };
+
+  const deleteCart = (cart) => {
+    const _carts = carts.filter((d) => d.id !== cart.id);
+    setCarts(_carts);
+  };
+  return (
+    <Layout>
+      <div className="cart-page">
+        <span className="cart-title">Shopping Cart</span>
+        <div className="cart-list">
+          <TransitionGroup component={null}>
+            {carts.map((cart) => (
+              <CSSTransition classNames="cart-item" timeout={300} key={cart.id}>
+                <CartItem
+                  cart={cart}
+                  key={cart.id}
+                  updateCart={updateCart}
+                  deleteCart={deleteCart}
+                />
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </div>
+        {carts.length === 0 ? <div className="no-cart">No items</div> : ""}
+
+        <div className="cart-total">
+          Total:
+          <span className="total-price">{totalPrice()}</span>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default Cart;
